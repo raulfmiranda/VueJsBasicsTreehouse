8ccdd23ce6fcb352ac4a7b9dcb339b28f82e3fda
RaulMirandaOSF <raul.miranda@osf-commerce.com>
2019-05-09 15:37:25 -0300

    Done all projects excepts Flashcard App

---
+++ rainbow/app.js
@@ -0,0 +1,9 @@
+const colorsOfTheRainbow = [
+    "red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+new Vue({
+    el: '#colors',
+    data: {
+        rainbow: colorsOfTheRainbow
+    }
+});
